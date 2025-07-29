1b12d77954b4010027a67fefc3b5882dbd8c7785
sozibhossain <sozibhossain69@gmail.com>
2025-07-29 11:47:02 +0600

    added

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import AllUsers from './_components/allUsers'
+import Layout from '@/components/dashboard/layout'
+
+function Page() {
+  return (
+    <Layout>
+       <AllUsers />
+    </Layout>
+  )
+}
+
+export default Page
